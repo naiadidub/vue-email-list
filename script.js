89946e4afce97e9182594100d4717c4e13ba9b48
naiadidub <antonio.zaghi1405@gmail.com>
2022-05-03 09:49:13 +0200

    tolta una proprietà di data inutile

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 const app = new Vue({
     el: '#app',
     data:{
-        email: '',
         emailList: []
     },
     methods: {},
     created(){
         for(let i = 0; i < 10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res)=> {
-                this.email = res.data.response
-                this.emailList.push(this.email)
+                
+                this.emailList.push(res.data.response)
             })
         }
     }
